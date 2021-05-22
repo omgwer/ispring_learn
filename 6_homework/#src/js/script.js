db61b2b13acbad3bad67fbbdd424a82dbd644cea
omgwer <hersgory2134@gmail.com>
2021-05-22 21:16:17 +0300

    upd

---
+++ 6_homework/#src/js/script.js
@@ -0,0 +1,36 @@
+window.addEventListener('load', function () {
+  setBodyLoaded();
+  setAnimatedBlockLoaded();
+});
+
+function setBodyLoaded() {
+  let body = document.getElementsByTagName('body')[0];
+  body.classList.add('loaded');
+}
+
+function setAnimatedBlockLoaded() {
+  const animatedBlockAdventages = document.getElementById('animatedBlockAdventages');
+  const animatedBlock = document.getElementById('animatedBlock');
+  if (!animatedBlock) {
+    return;
+  }
+  let reachedSecond = false;
+  let reached = false;
+  const ANIMATED_BLOCK_OFFSET = 300;
+  const ANIMATED_BLOCK_SECOND = 150;
+
+  window.addEventListener('scroll', () => {
+    if (window.pageYOffset + window.innerHeight > animatedBlock.offsetTop + ANIMATED_BLOCK_OFFSET) {
+      if (!reached) {
+        animatedBlock.classList.add('loaded');
+        reached = true;
+      }
+    }
+    if (window.pageYOffset + window.innerHeight > animatedBlockAdventages.offsetTop + ANIMATED_BLOCK_SECOND) {
+      if (!reachedSecond) {
+        animatedBlockAdventages.classList.add('loaded');
+        reachedSecond = true;
+      }
+    }
+  })
+}
